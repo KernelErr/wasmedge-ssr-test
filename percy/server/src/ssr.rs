d068501be97f837d4616a8cafc8d7c2d4b8fbee6
KernelErr <me@lirui.tech>
2022-02-25 17:33:59 +0800

    Remove unused library and struct
    Signed-off-by: KernelErr <me@lirui.tech>

--- percy/server/src/ssr.rs
+++ percy/server/src/ssr.rs
@@ -1,37 +1,7 @@
-use serde::Serialize;
 use isomorphic_app::App;
 
 const HTML_PLACEHOLDER: &str = "#HTML_INSERTED_HERE_BY_SERVER#";
 const STATE_PLACEHOLDER: &str = "#INITIAL_STATE_JSON#";
-
-#[derive(Serialize)]
-pub struct PercySSR {
-    click_count: u32,
-    path: String,
-    contributors: Option<Vec<PercyContributor>>,
-    has_initiated_contributors_download: bool,
-}
-
-impl PercySSR {
-    pub fn new(init: Option<u32>, path: String) -> Self {
-        Self {
-            click_count: init.unwrap_or(1001),
-            path: path.clone(),
-            contributors: None,
-            has_initiated_contributors_download: false,
-        }
-    }
-
-    pub fn times(&self) -> u32 {
-        self.click_count
-    }
-}
-
-#[derive(Serialize)]
-pub struct PercyContributor {
-    pub login: String,
-    pub html_url: String,
-}
 
 pub struct ServerSideRender;
 
